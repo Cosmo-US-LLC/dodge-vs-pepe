49e4c6c6380643e40b152800d5a42ca07356bfce
jawad <jawadakhtar@cosmoinc.co>
2024-12-21 07:01:09 +0500

    The rules & Prize money sections added

--- src/Page/MobileHome/MobileHome.jsx
+++ src/Page/MobileHome/MobileHome.jsx
@@ -3,16 +3,20 @@ import Navbar from "../../compunent/navbar";
 import MobileHeroSection from "./MobileHeroSection";
 import HeroWalletSec from "./HeroWalletSec";
 import MobileFirstEverSec from "./MobileFirstEverSec";
+import TheRules from "./TheRules";
+import PrizeMoney from "./PrizeMoney";
+
 function MobileHome() {
-   
   return (
     <div className="bg-[black] w-[100%]">
-    <div>
-      <Navbar />
-      <MobileHeroSection />
-      <HeroWalletSec />
-      <MobileFirstEverSec />
-    </div>
+      <div>
+        <Navbar />
+        <MobileHeroSection />
+        <HeroWalletSec />
+        <MobileFirstEverSec />
+        <TheRules />
+        <PrizeMoney />
+      </div>
     </div>
   );
 }
